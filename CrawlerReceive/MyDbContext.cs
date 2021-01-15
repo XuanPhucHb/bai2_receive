fa94ab79f48394b499e3b3d27b9eceefd57efb75
XuanPhucHb <xuanphuchb@gmail.com>
2021-01-15 20:50:35 +0700

    first commit

---
+++ CrawlerReceive/MyDbContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CrawlerReceive
+{
+    class MyDbContext : DbContext
+    {
+        public MyDbContext() : base("CrawReceivecontext")
+        {
+
+        }
+        public DbSet<Member> Members { get; set; }
+    }
+}
